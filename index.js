4db5a921b74b0ffbbf7d652d299473dca750a619
Suriya Prasath.S <48484191+SP-2605@users.noreply.github.com>
2020-02-26 16:08:47 +0530

    Optimized code

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ $(document).ready(function(){
                 gap: 2,
                 perView: 2,
                 peek: {
-                    before: 8,
-                    after: 8,
+                    before: 20,
+                    after: 20,
                 }
             },
 
@@ -89,37 +89,4 @@ $(document).ready(function(){
         }
     }
     new Glide(".glide1", config).mount()
-})
-
-
-//IMAGE OPTIMIZER
-$(document).ready(function () {
-    
-    $.cloudinary.image("logo_bqw7xa.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Main-page_nb0kll.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon-5_b9un2o.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon-4_wggwrh.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon-3_gasvp2.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon -3_gasvp2.jpg", { quality: "auto", fetch_format: "auto" });
-    
-    $.cloudinary.image("Ramkishan_Bhise_vyt1ze.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Pranita_Mahajan_pk1jyl.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("BADE_KRANTI_ficryn.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("BHAGYASHAL_WAGHMARE_yuidu0.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Namrata_Patel_lkxwve.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("GAURAV_FASATE_u5nmsu.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Ali_bl3rsb.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("man_tuxgth.png", { quality: "auto", fetch_format: "auto" });
-
-    $.cloudinary.image("Beacon - Electrician_tnsi4x.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - web - Dev -1_eowq8g.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - Welding_esu6xj.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - Sweaing_b30k7a.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - Comp - Opt_dyiv9x.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - AC_eiuxil.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - Screen - Printing_plsk8u.jpg", { quality: "auto", fetch_format: "auto" });
-    $.cloudinary.image("Beacon - Plumbing_uotfcq.jpg", { quality: "auto", fetch_format: "auto" });
-
-    $.cloudinary.image("Form - img_sw7ik2.jpg", { quality: "auto", fetch_format: "auto" });
-    
 })
